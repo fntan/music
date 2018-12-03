90c9b416a40ba951fee4c6414421518351a04ad8
fntan <1355834192@qq.com>
2018-12-03 23:23:59 +0800

    t

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+import fastclick from 'fastclick'
+
+fastclick.attach(document.body)
+
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
